37b07c29d509c5df6800584f27aee6cb6ee28c62
Gaozebin <32197390+Gaozebin@users.noreply.github.com>
2017-11-04 19:13:50 +0800

    Update practice-1.js

--- main/section-1/practice-1.js
+++ main/section-1/practice-1.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+        const collectionA = ['a', 'e', 'h', 't', 'f', 'c', 'g', 'b', 'd']; 
+        const collectionB = ['a', 'd', 'e', 'f']; 
+        var result = new Array();
+        var c=collectionB.toString();
+        for(var i=0 ; i < collectionA.length; i++ )
+        { 
+          if(c.indexOf(collectionA[i].tosting())>-1) 
+          {
+           result.push(collectionA[i]);
+          } 
+         }
+  return result;
 }
